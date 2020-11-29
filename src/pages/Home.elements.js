db938c35270a15b9ce220cf8d6b87e131d1cbbc0
fcrozatier <frederic.crozatier@protonmail.com>
2020-11-29 16:04:24 +0100

    Add 'Visit gallery' button on Home

--- src/pages/Home.elements.js
+++ src/pages/Home.elements.js
@@ -1,12 +1,35 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components/macro";
 
 export const Container = styled.div`
   display: flex;
   padding-top: 30px;
   justify-content: center;
+  position: relative;
 `;
 
 export const MainArtWork = styled.img`
+  display: flex;
   max-width: 90%;
+  max-height: 658px;
   object-fit: contain;
+  object-position: center center;
+`;
+
+export const BtnContainer = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+`
+
+export const Btn = styled(Link)`
+  position: relative;
+  top: -50%;
+  left: -50%;
+  background-color: #000;
+  color: white;
+  text-decoration: none;
+  padding: 20px;
+  border: 1px solid #fff;
+  user-select: none;
 `;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,16 @@
-import React from "react";
-import {
-  Container, MainArtWork,
-} from "./Home.elements";
+import { Btn, BtnContainer, Container, MainArtWork } from "./Home.elements";
 
 function Home() {
   return (
     <>
       <Container>
-          <MainArtWork src={require("../images/main.jpg").default} alt="showcase" />
+        <MainArtWork
+          src={require("../images/main.jpg").default}
+          alt="showcase"
+        />
+        <BtnContainer>
+          <Btn to="/shop">Visit gallery</Btn>
+        </BtnContainer>
       </Container>
     </>
   );
